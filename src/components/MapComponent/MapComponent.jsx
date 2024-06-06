5b9986698ef6290cd6cb5c1acbf22917b7d700ca
Abenezer Chane <abenezerchane@gmail.com>
2024-06-06 16:53:11 -0700

    fetching the api key from the back

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,31 +1,63 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { styled } from '@mui/system';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MapContainer = styled('div')({
-    height: '400px',
-    width: '100%',
-});
 
-const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY; // Importing API key from .env file
+
+  // Importing API key from .env file
 
 const MapComponent = () => {
+    const MapContainer = styled('div')({
+        height: '400px',
+        width: '100%',
+    });
+    const PORT = process.env.PORT || 8080;
+    const baseUrl = `http://localhost:${PORT}`;
+    
+    const [apiKey,setApiKey] = useState('');
+    const [isMapLoaded, setIsMapLoaded] = useState(false);
+
+
+
+    useEffect(() => { 
+        axios.get(`${baseUrl}/mapData`)
+        .then((res) => {
+            setApiKey(res.data.apiKey);
+            setDefaultCenter(res.data.location);
+            console.log(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    },
+    []);
+
+
+
+
     const mapStyles = {
         height: '100%',
         width: '100%',
     };
 
-    const defaultCenter = { // Default center of the map Vancouver
+    const [defaultCenter, setDefaultCenter ]= useState({ // Default center of the map Vancouver
         lat: 49.2827,
         lng: -123.1207
-      };
+      });
+
 
     return (
-        <LoadScript googleMapsApiKey={apiKey}>
-            <MapContainer>
+        apiKey ? (
+        <LoadScript googleMapsApiKey={apiKey}
+                    onLoad={() => setIsMapLoaded(true)}
+                    onError={() => console.log("Error getting map data.")}
+        >
+            {isMapLoaded? (<MapContainer>
                 <GoogleMap
                     center={defaultCenter}
-                    zoom={10}
+                    zoom={8}
                     options={{
                         disableDefaultUI: true,
                     }}
@@ -33,8 +65,8 @@ const MapComponent = () => {
                 >
                     <Marker position={defaultCenter} />
                 </GoogleMap>
-            </MapContainer>
-        </LoadScript>
+            </MapContainer>) : <div> Loading map ... </div>}
+        </LoadScript> ) : <div> Loading map ... </div>
     );
 };
 
